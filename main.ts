8e310789c207f6d278d60eb2ab2ba91bd8992ac2
charliegregg <64454803+charliegregg@users.noreply.github.com>
2020-05-20 10:17:33 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,39 @@
  * Dice Roller
  */
 enum Dice {
+    //% block=D2
     D2 = 2,
+    //% block=D4
     D4 = 4,
+    //% block=D6
     D6 = 6,
+    //% block=D8
     D8 = 8,
+    //% block=D10
     D10 = 10,
+    //% block=D12
     D12 = 12,
+    //% block=D20
     D20 = 20,
+    //% block=D100
     D100 = 100
 }
+enum TropicalFruit {
+    //% block=banana
+    Banana = 0,
+    //% block=pineapple
+    Pinapple = 1,
+    //% block=coconut
+    Coconut = 2
+}
 //% color="#ad4800" icon="\u2684" block="Dice" weight=42
-namespace RollingDice {
+namespace DiceRolling {
     /** Rolls a D6
      * @param Times    The times to roll
      */
     //% blockId=wowie block="Roll a D6 $Times times"
     //% group="Preset" weight=91
-    export function RollD6(Times: number): number {
+    export function RollaD6(Times: number): number {
         let Sum = 0
         for (let index = 0; index < Times; index++) {
             Sum = Sum + Math.randomRange(1, 6)
@@ -39,4 +55,4 @@ namespace RollingDice {
         return (Sum2)
     }
 }
-game.splash(convertToText(RollingDice.RollCustom(1, Dice.D20)))
+game.splash(convertToText(DiceRolling.RollCustom(1, Dice.D20)))
